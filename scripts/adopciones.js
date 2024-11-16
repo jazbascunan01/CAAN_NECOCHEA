d667b30e95771af608cb72e71a53d9f1d6552685
Mateo Fernandez <emfernandez@alumnos.exa.unicen.edu.ar>
2024-11-16 17:58:26 -0300

    Boton "ver más" en cards de perros

--- scripts/adopciones.js
+++ scripts/adopciones.js
@@ -1,20 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
-fetch('./utils/perros.json')
-  .then(response => response.json())
-  .then(perros => {
-    const contenedor = document.getElementById('contenedor-perros');
-    perros.forEach(perro => {
-      const tarjeta = document.createElement('div');
-      tarjeta.classList.add('tarjeta-perro');
-      tarjeta.innerHTML = `
-        <img src="${perro.imagen}" alt="Imagen de ${perro.nombre}">
-        <h3>${perro.nombre}</h3>
-        <p>Raza: ${perro.raza}</p>
-        <p>Edad: ${perro.edad} años</p>
-        <p>${perro.descripcion}</p>
-      `;
-      contenedor.appendChild(tarjeta);
-    });
-  })
-  .catch(error => console.error('Error cargando los datos:', error));
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('./utils/perros.json')
+    .then(response => response.json())
+    .then(perros => {
+      const contenedor = document.getElementById('contenedor-perros');
+      perros.forEach(perro => {
+        const tarjeta = document.createElement('div');
+        tarjeta.classList.add('tarjeta-perro');
+        tarjeta.innerHTML = `
+          <img src="${perro.imagen}" alt="Imagen de ${perro.nombre}">
+          <h3>${perro.nombre}</h3>
+          <p>Raza: ${perro.raza}</p>
+          <p>Edad: ${perro.edad} años</p>
+          <p>${perro.descripcion}</p>
+          <button class="adoption-button" onclick="window.location.href='adoptar/${perro.id}'">Ver más</button>
+        `;
+        contenedor.appendChild(tarjeta);
+      });
+    })
+    .catch(error => console.error('Error cargando los datos:', error));
 });
