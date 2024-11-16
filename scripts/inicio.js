5df7952ec8b684f7bfee658e6a22dc541e0b9782
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-11-16 11:58:48 -0300

    urls reparadas

---
+++ scripts/inicio.js
@@ -0,0 +1,6 @@
+document.querySelector('.adopt-card').addEventListener('click', function() {
+    window.location.href = 'adoptar.html';
+});
+document.querySelector('.event-card').addEventListener('click', function() {
+    window.location.href = 'eventos.html';
+});
