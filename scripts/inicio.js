aa9a6646e26f1dfc4fd5eef2043596dcb3ce871e
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-11-27 10:30:42 -0300

    animaciones agregadas

--- scripts/inicio.js
+++ scripts/inicio.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init();
+    new WOW().init();
     // Verificar si el usuario está logueado
     const user = JSON.parse(localStorage.getItem('loggedInUser'));
 
@@ -19,16 +21,16 @@ document.addEventListener('DOMContentLoaded', function() {
             <a href="Registro.html" class="cta-button">Registrarse</a>
         `;
     }
-document.querySelector('.adopt-card').addEventListener('click', function() {
-    window.location.href = 'adoptar.html';
-});
-document.querySelector('.event-card').addEventListener('click', function() {
-    window.location.href = 'eventos.html';
-});
-document.querySelector('.volunteer-card').addEventListener('click', function() {
-    window.location.href = 'login.html';
-});
-document.querySelector('.donar-card').addEventListener('click', function() {
-    window.location.href = 'donar.html';
-});
+    document.querySelector('.adopt-card').addEventListener('click', function () {
+        window.location.href = 'adoptar.html';
+    });
+    document.querySelector('.event-card').addEventListener('click', function () {
+        window.location.href = 'eventos.html';
+    });
+    document.querySelector('.volunteer-card').addEventListener('click', function () {
+        window.location.href = 'login.html';
+    });
+    document.querySelector('.donar-card').addEventListener('click', function () {
+        window.location.href = 'donar.html';
+    });
 });
