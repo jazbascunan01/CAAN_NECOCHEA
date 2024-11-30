c4d0e1af75e57b8e25b53b95c004a2cb417e6e2b
Jazmin Bascuñan <jazbascunan01@gmail.com>
2024-11-30 12:00:14 -0300

    Había faltado desarrollar la parte de la confirmación de la donacion de dinero

--- scripts/donar.js
+++ scripts/donar.js
@@ -1,18 +1,30 @@
 const metodoPago = document.getElementById("metodo-pago");
-        const tarjetaInfo = document.getElementById("tarjeta-info");
-        const transferenciaInfo = document.getElementById("transferencia-info");
-        const paypalInfo = document.getElementById("paypal-info");
+const tarjetaInfo = document.getElementById("tarjeta-info");
+const transferenciaInfo = document.getElementById("transferencia-info");
+const paypalInfo = document.getElementById("paypal-info");
 
-        metodoPago.addEventListener("change", function () {
-            tarjetaInfo.style.display = "none";
-            transferenciaInfo.style.display = "none";
-            paypalInfo.style.display = "none";
+metodoPago.addEventListener("change", function () {
+    tarjetaInfo.style.display = "none";
+    transferenciaInfo.style.display = "none";
+    paypalInfo.style.display = "none";
 
-            if (metodoPago.value === "tarjeta") {
-                tarjetaInfo.style.display = "block";
-            } else if (metodoPago.value === "transferencia") {
-                transferenciaInfo.style.display = "block";
-            } else if (metodoPago.value === "paypal") {
-                paypalInfo.style.display = "block";
-            }
-        });
+    if (metodoPago.value === "tarjeta") {
+        tarjetaInfo.style.display = "block";
+    } else if (metodoPago.value === "transferencia") {
+        transferenciaInfo.style.display = "block";
+    } else if (metodoPago.value === "paypal") {
+        paypalInfo.style.display = "block";
+    }
+});
+document.getElementById("submit-form").addEventListener("click", function (event) {
+    event.preventDefault();
+    document.getElementById("step-1donacion").style.display = "none";
+    document.getElementById("formulario-don").style.display = "none";
+    document.getElementById("formulario-d").style.display = "none";
+    document.getElementById("step-5").style.display = "block";
+});
+
+document.getElementById("back-home").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.location.href = "/index.html";
+});
